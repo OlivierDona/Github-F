26da60e3d525d0c7930ea4a568f12e4cad18dc10
Jan Plotzki <jan.plotzki@tu-dortmund.de>
2025-02-14 22:08:24 +0100

    Letzte Funktion der Aufgabe 2 und zwei Hilsfunktionen

--- Funktionen-R-Skript 1.R
+++ Funktionen-R-Skript 1.R
@@ -1,8 +1,8 @@
 ##Funktion für deskriptive Statistiken metrischer Variablen
 
-decribe_metric <- function(data,variables) {
+describe_metric <- function(data,variables) {
   summary_stats <- data %>%
-    select(all_of(varaiables)) %>%
+    select(all_of(variables)) %>%
     summarise_all(list(
       Mittelwert = mean,
       Median = median,
@@ -52,4 +52,35 @@ bivariate_metric_dichotomous <- function(data, metric_var, dichotomous_var) {
   t_test_result <- t.test(data[[metric_var]] ~ data[[dichotomous_var]], data = data, na.action = na.omit)
   
   return(t_test_result)
+}
+
+## Funktion zur Visualisierung von 3-4 kategorialen Variablen
+plot_categorical <- function(df, var1, var2, var3, var4 = NULL) {
+  vars <- c(var1, var2, var3, var4)
+  vars <- vars[!sapply(vars, is.null)]  # Entferne NULL-Werte
+  
+  # Erstelle proportionale Tabelle
+  prop_table <- create_prop_table(df, vars)
+  
+  # Erstelle Plot
+  p <- ggplot(prop_table, aes_string(x = var1, y = "prop", fill = var2)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    facet_grid(paste(var3, "~", ifelse(is.null(var4), ".", var4))) +
+    labs(y = "Proportion", fill = var2) +
+    theme_minimal()
+  
+  print(p)
+}
+
+## Funktion für ein Streudiagramm mit Regressionsgerade für zwei metrische Variablen
+plot_metric_correlation <- function(data,var1,var2){
+  corr_as_text = calc_correlation(data[[var1]],data[[var2]])
+  
+  p = ggplot(data,aes_string(x = var1,y = var2)) + geom_point(color = "blue") + 
+               geom_smooth(method = "lm",color = "red") + 
+               annotate("text",x = Inf, y = Inf, label = corr_as_text, hjust = 1, vjust = 1.5, size = 5) + 
+    labs(title = paste("Korrelation zwischen",var1,"und",var2), x = var1, y = var2) +
+    theme_minimal()
+  
+  print(p)
 }
---
+++ Funktionen-R-Skript 2.R
@@ -0,0 +1,14 @@
+# Hilfsfunktion zum Erstellen einer Häufigkeitstabelle 
+create_prop_table <- function(df, vars) {
+  df %>%
+    count(across(all_of(vars))) %>%
+    group_by(across(all_of(vars[-length(vars)]))) %>%
+    mutate(prop = n / sum(n)) %>%
+    ungroup()
+}
+
+# Hilfsfunktion zur Berechnung der Korrelation und Rückgabe als Text
+calc_correlation <- function(x, y) {
+  corr_value <- cor(x, y, use = "complete.obs")
+  return(paste0("Korrelation: r = ", round(corr_value, 2)))
+}
